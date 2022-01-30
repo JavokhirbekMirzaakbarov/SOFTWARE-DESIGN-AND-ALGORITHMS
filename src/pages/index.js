039ba9f8582be9ddecde674101fe77ab578b4863
Denys Kybka <denys_kybka@epam.com>
2022-01-30 13:13:17 +0000

    Merge oop modules

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,5 +2,5 @@ import React from 'react';
 import {Redirect} from '@docusaurus/router';
 
 export default function Home() {
-  return <Redirect to="/docs/OBJECT-ORIENTED_PROGRAMMING_AND_OBJECT-ORIENTED_DESIGN/CONTENTS" />;
+  return <Redirect to="/docs/contents" />;
 }
