36deb7340355c46fa952d984cf5fb1276b84aff2
Javokhirbek Mirzaakbarov <javokhirbek_mirzaakbarov@epam.com>
2023-08-31 15:38:10 +0500

    8-module completed

--- docs/8-algorithms-and-data-structures-part-2/hometask/src/Scheduler.ts
+++ docs/8-algorithms-and-data-structures-part-2/hometask/src/Scheduler.ts
@@ -5,4 +5,33 @@ export interface SchedulerI {
   run(): Promise<void>;
 }
 
-export class Scheduler implements SchedulerI {}
+export class Scheduler implements SchedulerI {
+  private taskQueue: PriorityQueue<{
+    task: () => Promise<any>;
+    priority: number;
+    order: number;
+  }> = new PriorityQueue();
+  private taskCounter: number = 0;
+  private isRunning: boolean = false;
+
+  postTask(task: () => Promise<any>, priority: number): void {
+    this.taskQueue.enqueue(
+      { task, priority, order: this.taskCounter },
+      priority * 1000 + this.taskCounter
+    );
+    this.taskCounter++;
+  }
+
+  async run(): Promise<void> {
+    if (this.isRunning) return;
+
+    this.isRunning = true;
+
+    while (this.taskQueue.size() > 0) {
+      const { task } = this.taskQueue.dequeue()!;
+      await task();
+    }
+
+    this.isRunning = false;
+  }
+}
